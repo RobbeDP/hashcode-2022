59a1a080f186533f905fdccafad1c41f894f0b7d
Karel Lievens <karel.lievens@ugent.be>
2022-02-24 22:17:51 +0100

    finished final final

--- qualification/parse.py
+++ qualification/parse.py
@@ -26,15 +26,52 @@ class Project:
             repr += f"\t{skill} with level {level} required\n"
         return repr
 
-class Graph:
+
+class ProjectGraph:
     def __init__(self, project, contributors):
-        self.roles = project.skills.items()
-        self.fulfilled = dict()
-        self.can_learn = dict()
+        self.name = project.name
+        self.days = project.days
+        self.required_roles = dict() # project.skills.items()
+        self.assignment = set()
+        for name, level in project.skills.items():
+            self.required_roles[name] = {'fulfilled': set(), 'mentor': set(), 'learn': set()}
+            for contributor in contributors:
+                if contributor.skills[name] > level:
+                    self.required_roles[name]['fulfilled'].add(contributor.name)
+                elif contributor.skills[name] == level-1:
+                    self.required_roles[name]['mentor'].add(contributor.name)
+                elif contributor.skills[name] == level:
+                    self.required_roles[name]['learn'].add(contributor.name)
 
-        for contributor in contributors:
-            pass
+    def assign(self, contributor):
+        self.assignment.add(contributor.name)
 
+    def num_edges(self, key):
+        return sum([len(requirement[key]) for requirement in self.required_roles.values()])
+
+    def update(self, learned): # learned: [(contributor, role)]
+        for contributor, role in learned:
+            if contributor in self.required_roles[role]['mentor']:
+                self.required_roles[role]['mentor'].remove(contributor)
+                self.required_roles[role]['learn'].add(contributor)
+            elif contributor in self.required_roles[role]['learn']:
+                self.required_roles[role]['learn'].remove(contributor)
+                self.required_roles[role]['fulfilled'].add(contributor)
+
+    def schedule(self):
+        learned = set()
+        for role, edges in self.required_roles.items():
+            learn_non_ass = list(edges['learn'] - self.assignment)
+            if len(learn_non_ass) > 0:
+                self.assign(learn_non_ass[0])
+                learned.add((learn_non_ass[0], role))
+                continue
+            fulfilled_non_ass = list(edges['fulfilled'] - self.assignment)
+            if len(fulfilled_non_ass) > 0:
+                self.assign(fulfilled_non_ass[0])
+            else:
+                return False
+        return self.assignment, learned
 
 def parse(filename):
     with open(filename) as input_file:
@@ -77,7 +114,22 @@ def parse(filename):
     return projects, contributors
 
 def schedule(projects, contributors):
-    schedule = dict()
+    graphs = [ProjectGraph(project, contributors) for project in projects]
+    graphs.sort(key=lambda x: (x.num_edges('learn'), x.num_edges('mentor'), x.days), reverse=True)
+    prev_length = len(graphs) + 1
+    while len(graphs) > 0 and prev_length > len(graphs):
+        scheduled = list()
+        for graph in graphs:
+            scheduling = graph.schedule()
+            if not scheduling:
+                continue
+            scheduled.append(graph.name)
+            
+            for graph in graphs:
+                if graph.name not in scheduled:
+                    graph.update(learned)
+                
+        
 
 
 if __name__ == "__main__":
@@ -85,5 +137,5 @@ if __name__ == "__main__":
     s = schedule(projects, contributors)
     # for project, assignments in s.items():
     #     print(f"Project {project}: {', '.join(assignments.values())}")
-    write_output(s)
+    # write_output(s)
 
