39105ba58dc9ca3327ccfe0112eb1da7df01cd9b
Karel Lievens <karel.lievens@ugent.be>
2022-02-24 19:03:33 +0100

    classes

---
+++ qualification/main.py
@@ -0,0 +1,13 @@
+class Contributor:
+    def __init__(self, name, skills):
+        self.name = name
+        self.skills = skills
+
+class Project:
+    def __init__(self, skills, days, score, bb):
+        self.skills = skills
+        self.days = days
+        self.score = score
+        self.bb = bb
+
+
